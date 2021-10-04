dbe1e650936881d92c7a5ea83ea13591066b474f
Personal <ulasgokce98@gmail.com>
2021-10-04 16:32:52 +0300

    fix

--- src/DetectCMS/Systems/Concrete5.php
+++ src/DetectCMS/Systems/Concrete5.php
@@ -26,8 +26,7 @@ class Concrete5 extends \DetectCMS\DetectCMS
         if ($this->isValid()) {
             require_once __DIR__ . '/../Thirdparty/simple_html_dom.php';
 
-            $html = str_get_html($this->home_html);
-            if (!empty($html)) {
+          if($html = str_get_html($this->home_html)) {
                 if ($generator = $html->find("meta[name='generator']", false)) {
                     return stripos($generator->content, 'concrete5') !== false;
                 }
